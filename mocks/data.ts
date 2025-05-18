30bea6f8a8f9956e8b594824f3424c641b658692
Vinicius_FPach <94939002+Vinicius-FPach@users.noreply.github.com>
2025-05-18 16:35:43 -0300

    install and config expo-router (#6)

---
+++ mocks/data.ts
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    carrer: "Fiorentina",
+    team: "Fiorentina",
+    season: 10,
+  },
+  {
+    carrer: "Champions França",
+    team: "PSG",
+    season: 4,
+  },
+  {
+    carrer: "Libertadores",
+    team: "River Plate",
+    season: 6,
+  },
+];
